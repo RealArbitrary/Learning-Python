b3a22fdf11c29475f7ebaeed1b2e94e18262174b
De Wet van der Merwe <49815767+RealArbitrary@users.noreply.github.com>
2021-10-24 13:51:10 +0200

    Split up files properly

---
+++ Block Letters.py
@@ -0,0 +1,21 @@
+#De Wet is my name
+#I am lazy but this makes me a hard worker
+name = """
+DDDD
+D     D
+D     D
+D     D
+D     D
+D     D
+DDDDD
+"""
+name += """
+w     w
+w     w
+w     w
+w  w  w
+ww   ww
+ww   ww
+w     w
+"""
+print(name)
