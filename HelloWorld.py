5f1e2e5f1659bad8b649114f1be31b0ff89b5fb2
De Wet van der Merwe <49815767+RealArbitrary@users.noreply.github.com>
2021-10-24 13:48:19 +0200

    Initial commit

---
+++ HelloWorld.py
@@ -0,0 +1,25 @@
+#Just getting into it here
+cool_number = 12 + 30
+cool_number * 5
+print(cool_number)
+#De Wet is my name
+#I am lazy but this makes me a hard worker
+name = """
+DDDD
+D     D
+D     D
+D     D
+D     D
+D     D
+DDDDD
+"""
+name += """
+w     w
+w     w
+w     w
+w  w  w
+ww   ww
+ww   ww
+w     w
+"""
+print(name)
