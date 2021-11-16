25824615d5205542139c8c0453ecdebd9167a701
De Wet van der Merwe <49815767+RealArbitrary@users.noreply.github.com>
2021-11-16 20:21:29 +0200

    Finished Lists Exercise
    Stopped before next project. Also added Project folder for the Project to come

--- Lists Module/Module Exercises/6. Growing a List - Plus (+).py
+++ Lists Module/Module Exercises/6. Growing a List - Plus (+).py
@@ -28,3 +28,26 @@
 # my_list + [4]
 # Let’s use + to practice combining two lists!
 
+# Jiho is updating a list of orders. He just received orders for "lilac" and "iris".
+
+# Create a list called new_orders that contains our new orders.
+
+# Checkpoint 2 Passed
+
+# Use + to create a new list called orders_combined that combines orders with new_orders.
+
+# Checkpoint 3 Passed
+
+# Remove the # and whitespace in front of the list broken_prices. If you run this code, you’ll get an error:
+
+# TypeError: can only concatenate list (not "int") to list
+# Fix the command by inserting brackets ([ and ]) so that it will run without errors.
+
+# Solution to three objective above: (I forgot where I was and continued after long so this file is a bit weird compared to the rest)
+# orders = ["daisy", "buttercup", "snapdragon", "gardenia", "lily"]
+
+# # Create new orders here:
+# new_orders = ["lilac", "iris"]
+# orders_combined = orders + new_orders
+
+# broken_prices = [5, 3, 4, 5, 4] + [4]
